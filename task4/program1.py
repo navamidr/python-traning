5657be34cfb8f1faddd27c6837680f8252073ede
navamidr <navamidr@gmail.com>
2024-11-28 15:41:39 +0530

    task4 answers

---
+++ task4/program1.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+a = np.array([1, 2, 3])
+b = np.array([4,5,6])
+
+#addition, subtraction, multiplication, and division.
+
+print("Addition:" ,a + b)
+
+print("Subtraction:" ,a-b)
+
+print("Multiplication:", a * b) 
+
+print("Division:", a/b)
---
+++ task4/program2.py
@@ -0,0 +1,5 @@
+
+import numpy as np 
+r = np.random.normal(size=100) 
+mean = np.mean(r)
+print("Mean of the random values:",mean)
